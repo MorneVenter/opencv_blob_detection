e5a124539197615b6e9bbece25817dbc72850c01
Morne Venter <morneventer@pop-os.localdomain>
2020-02-06 12:43:47 +0200

    added sunspot detection

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import cv2
 import matplotlib.pyplot as plt
 
-img = cv2.imread('img.jpg', 0)
-surf = cv2.xfeatures2d.SURF_create(50000) #create the SURF object
+for i in range(4):
+    print('####################################################################')
+    print('IMAGE ' + str(i+1))
+    print('####################################################################')
+    img = cv2.imread('spot_'+str(i+1)+'.jpg', 0)
+    surf = cv2.xfeatures2d.SURF_create(7500) #create the SURF object
 
-kp, des = surf.detectAndCompute(img, None) #find keypoints and descriptors
+    if not(surf.getUpright()):
+        surf.setUpright(True)
+    if not(surf.getExtended()):
+        surf.setExtended(True)
 
-final_img = cv2.drawKeypoints(img,kp,None,(255,0,0),4) # draws keypoints to image
-plt.imshow(final_img), plt.show()
+    kp, des = surf.detectAndCompute(img, None) #find keypoints and descriptors
+    print('Size of descriptors: ' + str(surf.descriptorSize()))
+    print('Number of keypoints: ' + str(len(kp)))
+    final_img = cv2.drawKeypoints(img,kp,None,(255,0,0),4) # draws keypoints to image
+    for point in kp:
+        print('Sunspot at:' + str(round(point.pt[0],2)) +' ' + str(round(point.pt[1],2)))
+    plt.imshow(final_img), plt.show()
+    print('####################################################################')
