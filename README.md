548d9ef5433e0e63f7d8223d12af6c26cf83780b
Morne Venter <morneventer@pop-os.localdomain>
2020-02-06 11:59:21 +0200

    display imagae

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Opencv blob detection
 Detecting blobs using surf.
 
-## Install Opencv
+## Install Opencv nd other packages
 ```sudo pip install opencv-python==3.3.0.10 opencv-contrib-python==3.3.0.10```
+```pip install matplotlib==2.0.2```
+```sudo apt install python-tk```
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import cv2
+import matplotlib.pyplot as plt
 
 img = cv2.imread('img.jpg', 0)
 surf = cv2.xfeatures2d.SURF_create(50000) #create the SURF object
 
 kp, des = surf.detectAndCompute(img, None) #find keypoints and descriptors
+
+final_img = cv2.drawKeypoints(img,kp,None,(255,0,0),4) # draws keypoints to image
+plt.imshow(final_img), plt.show()
